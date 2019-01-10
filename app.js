6254ffb629561522404bc1c5585babb5a7d1e22e
John Bull <91671@savemart.com>
2019-01-10 13:41:52 -0800

    Commiting test app

---
+++ app.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const parse = require("csv-parse");
+
+const kafka = require("kafka-node");
+const Producer = kafka.Producer;
+const client = new kafka.Client("localhost:2181");
+
+const testTopic = "test";
+
+(KeyedMessage = kafka.KeyedMessage),
+  (producer = new Producer(client)),
+  (km = new KeyedMessage("key", "message")),
+  (testProducerReady = false);
+
+producer.on("ready", function() {
+  console.log("Producer for tests is ready");
+  testProducerReady = true;
+});
+
+producer.on("error", function(err) {
+  console.error("Problem with producing Kafka message " + err);
+});
+
+const inputFile = "testReport.csv";
+var averageDelay = 3000; // in miliseconds
+var spreadInDelay = 2000; // in miliseconds
+
+var dataArray = [];
+
+var parser = parse({ delimiter: "," }, function(err, data) {
+  dataArray = data;
+  handleData(1);
+});
+
+fs.createReadStream(inputFile).pipe(parser);
+
+function handleData(currentData) {
+  let line = dataArray[currentData];
+  let dataNode = {
+    store: line[0],
+    code: line[1],
+    RXCount: line[5]
+  };
+  console.log(JSON.stringify(dataNode));
+  produceDataMessage(dataNode);
+  let delay = averageDelay + (Math.random() - 0.5) * spreadInDelay;
+  setTimeout(handleData.bind(null, currentData + 1), delay);
+}
+
+function produceDataMessage(dataNode) {
+  (KeyedMessage = kafka.KeyedMessage),
+    (dataNodeKM = new KeyedMessage(dataNode.code, JSON.stringify(dataNode))),
+    (payloads = [{ topic: testTopic, messages: dataNodeKM, partition: 0 }]);
+  if (testProducerReady) {
+    producer.send(payloads, function(err, data) {
+      console.log(data);
+    });
+  } else {
+    console.error(
+      "Sorry, TestProducer is not ready yet, failed to produce message to kafka"
+    );
+  }
+}
