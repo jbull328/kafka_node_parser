eaa70d2eb579efa32c80dde9a39b5b2bfea12f83
John Bull <91671@savemart.com>
2019-01-17 14:59:16 -0800

    imported json dumps,added it to producer statment.

--- employee_simple_producer.py
+++ employee_simple_producer.py
@@ -1,15 +1,19 @@
 from kafka import KafkaProducer, KafkaClient
 from kafka.errors import KafkaError
+from json import dumps
 # from confluent_kafka import avro
 # from confluent_kafka.avro import AvroProducer
 import csv
 import pandas as pd
 
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+
 
 topic = 'employees'
 
 employee_data = pd.read_csv('MOCK_Employee_DATA.csv')
 
-producer.send(topic, employee_data)
+producer.send(topic, value=employee_data)
