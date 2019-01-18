243c2a30f6d2592f3497fbdf74bd892bc5cf8927
John Bull <91671@savemart.com>
2019-01-18 11:01:05 -0800

    added some more parsing flags

--- employee_simple_producer.py
+++ employee_simple_producer.py
@@ -1,14 +1,13 @@
 from kafka import KafkaProducer, KafkaClient
 from kafka.errors import KafkaError
 from json import dumps
-# from confluent_kafka import avro
-# from confluent_kafka.avro import AvroProducer
 import csv
 import pandas as pd
 
 
-producer = KafkaProducer(
-    bootstrap_servers='localhost:9092', api_version=(0, 10))
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'), api_version=(0, 10))
 
 
 topic = 'employees'
