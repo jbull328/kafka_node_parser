9f22d2bc898cc2848a53f1284cb01ebf79117797
John Bull <91671@savemart.com>
2019-01-18 10:03:19 -0800

    worked with output data formatting

--- employee_simple_producer.py
+++ employee_simple_producer.py
@@ -15,7 +15,6 @@ topic = 'employees'
 
 employee_data = pd.read_csv('MOCK_Employee_DATA.csv').to_json(
     orient='records', lines=True)
-
 print(employee_data)
 
 producer.send(topic, value=employee_data)
