53b288042678fbf9ef3a800ad80b2a10b0dbddcb
John Bull <91671@savemart.com>
2019-01-18 15:00:31 -0800

    removed the default id

--- employeeConnector.js
+++ employeeConnector.js
@@ -4,11 +4,6 @@ const {
 } = require("sequelize-kafka-connect");
 
 const tableSchema = {
-  id: {
-    type: "integer",
-    allowNull: false,
-    primaryKey: true
-  },
   f_name: {
     type: "varchar(255)",
     allowNull: false
@@ -27,7 +22,6 @@ const etlFunc = (messageValue, callback) => {
   //type is an example json format field
   if (messageValue.type === "publish") {
     return callback(null, {
-      id: messageValue.payload.id,
       f_name: messageValue.payload.f_name,
       l_name: messageValue.payload.l_name,
       hire_date: messageValue.payload.hire_date
@@ -61,51 +55,3 @@ runSinkConnector(config, [converter], onError).then(config => {
         type: "publish"
     }
 */
-
-const config = {
-  kafka: {
-    noptions: {
-      "metadata.broker.list": "localhost:9092",
-      "group.id": "n-test-group",
-      "enable.auto.commit": false,
-      debug: "all",
-      event_cb: true,
-      "client.id": "kcs-test"
-    },
-    tconf: {
-      "auto.offset.reset": "earliest",
-      "request.required.acks": 1
-    }
-  },
-
-  topic: "sc_test_topic",
-  partitions: 1,
-  maxTasks: 1,
-  pollInterval: 2000,
-  produceKeyed: true,
-  produceCompressionType: 0,
-  connector: {
-    options: {
-      host: "localhost",
-      port: 5432,
-      dialect: "sqlite",
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      },
-      storage: path.join(__dirname, "test-db.sqlite")
-    },
-    database: null,
-    user: null,
-    password: null,
-    maxPollCount: 50,
-    table: "accounts",
-    incrementingColumnName: "id"
-  },
-  http: {
-    port: 3149,
-    middlewares: []
-  },
-  enableMetrics: true
-};
