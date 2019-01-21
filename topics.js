4fbfbbc50770e7113186bf9a58176b170de543f9
John Bull <91671@savemart.com>
2019-01-21 14:45:46 -0800

    fixed variables

--- topics.js
+++ topics.js
@@ -1,6 +1,6 @@
-var zk = require("node-zookeeper-client");
+var zookeeper = require("node-zookeeper-client");
 
-var client = zk.createClient("localhost:2181");
+var client = zookeeper.createClient("localhost:2181");
 
 client.zk.client.getChildren("/brokers/topics", (err, children, stats) => {
   children.forEach(child => console.log(child, stats));
