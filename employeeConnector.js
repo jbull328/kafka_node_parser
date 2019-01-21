119660ea83226c1c0bb564f2100ca90e9cc75dbc
John Bull <91671@savemart.com>
2019-01-21 09:35:12 -0800

    Added config for the connector to the mysql db

--- employeeConnector.js
+++ employeeConnector.js
@@ -3,6 +3,66 @@ const {
   ConverterFactory
 } = require("sequelize-kafka-connect");
 
+const config = {
+  kafka: {
+    //zkConStr: "localhost:2181/",
+    kafkaHost: "localhost:9092",
+    logger: null,
+    // groupId: "kc-sequelize-test",
+    clientName: "kc-sequelize-test-name",
+    workerPerPartition: 1,
+    options: {
+      sessionTimeout: 8000,
+      protocol: ["roundrobin"],
+      fromOffset: "earliest", //latest
+      fetchMaxBytes: 1024 * 100,
+      fetchMinBytes: 1,
+      fetchMaxWaitMs: 10,
+      heartbeatInterval: 250,
+      retryMinTimeout: 250,
+      requireAcks: 1
+      //ackTimeoutMs: 100,
+      //partitionerType: 3
+    }
+  },
+  topic: "employees",
+  partitions: 1,
+  maxTasks: 1,
+  pollInterval: 2000,
+  produceKeyed: true,
+  produceCompressionType: 0,
+  connector: {
+    options: {
+      host: "localhost",
+      port: 5432,
+      dialect: "mysql",
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+      storage: path.join(__dirname, "test-db.mysql")
+    },
+    database: null,
+    user: null,
+    password: null,
+    maxPollCount: 50,
+    table: "emplo",
+    incrementingColumnName: "id"
+  },
+  http: {
+    port: 3149,
+    middlewares: []
+  },
+  enableMetrics: true,
+  batch: {
+    batchSize: 100,
+    commitEveryNBatch: 1,
+    concurrency: 1,
+    commitSync: true
+  }
+};
+
 const tableSchema = {
   f_name: {
     type: "varchar(255)",
