4c35f8d87b4f3a73c3640f9f80e2e51eb65a0652
John Bull <91671@savemart.com>
2019-01-22 14:01:58 -0800

    got config set

--- employee_event_consumer.py
+++ employee_event_consumer.py
@@ -1,10 +1,12 @@
 from kafka import KafkaConsumer
 from kafka.errors import KafkaError
 import json
+from json import dumps
 
-consumer = KafkaConsumer('employees', bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer('employees', bootstrap_servers=['localhost:9092'],
+                         value_deserializer=lambda m:
+                         dumps(m).encode('utf-8'), api_version=(0, 10))
 
-KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii')))
 
 for message in consumer:
-    print(message.value)
+    print(message)
