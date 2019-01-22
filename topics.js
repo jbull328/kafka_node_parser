01177377604cc00c9525ff9013aa61eacac0244c
John Bull <91671@savemart.com>
2019-01-21 16:38:14 -0800

    trying to get a list

--- topics.js
+++ topics.js
@@ -1,33 +1,36 @@
 var zookeeper = require("node-zookeeper-client");
 
 var client = zookeeper.createClient("localhost:2181");
-var path = process.argv[2];
 
-function listChildren(client, path) {
-  client.getChildren(
-    path,
-    function(event) {
-      console.log("Got watcher event: %s", event);
-      listChildren(client, path);
-    },
-    function(error, children, stat) {
-      if (error) {
-        console.log(
-          "Failed to list children of node: %s due to: %s.",
-          path,
-          error
-        );
-        return;
-      }
+client.getChildren(console.log(data));
 
-      console.log("Children of node: %s are: %j.", path, children);
-    }
-  );
-}
+// var path = process.argv[2];
 
-client.once("connected", function() {
-  console.log("Connected to ZooKeeper.");
-  listChildren(client, path);
-});
+// function listChildren(client, path) {
+//   client.getChildren(
+//     path,
+//     function(event) {
+//       console.log("Got watcher event: %s", event);
+//       listChildren(client, path);
+//     },
+//     function(error, children, stat) {
+//       if (error) {
+//         console.log(
+//           "Failed to list children of node: %s due to: %s.",
+//           path,
+//           error
+//         );
+//         return;
+//       }
+
+//       console.log("Children of node: %s are: %j.", path, children);
+//     }
+//   );
+// }
+
+// client.once("connected", function() {
+//   console.log("Connected to ZooKeeper.");
+//   listChildren(client, path);
+// });
 
 client.connect();
