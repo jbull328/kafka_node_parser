5e50c2f14fbf8f73185646175685ebafe9b3b36f
John Bull <91671@savemart.com>
2019-01-22 14:49:59 -0800

    added main function to run script

--- employee_event_consumer.py
+++ employee_event_consumer.py
@@ -17,3 +17,21 @@ class Consumer(threading.Thread):
         consumer.subscribe(['employees'])
         for message in consumer:
             print(message)
+
+
+def main():
+    threads = [
+        Consumer()
+    ]
+    for t in threads:
+        t.start()
+    time.sleep(10)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:' +
+               '%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+    )
+    main()
