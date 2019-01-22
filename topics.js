b22a0abdab2ca0ad191770b9a669abefcc916a2d
John Bull <91671@savemart.com>
2019-01-21 16:00:15 -0800

    added full topic watcher

--- topics.js
+++ topics.js
@@ -1,11 +1,33 @@
 var zookeeper = require("node-zookeeper-client");
 
 var client = zookeeper.createClient("localhost:2181");
+var path = process.argv[2];
 
-client.zk.client.getChildren("/brokers/topics", (err, children, stats) => {
-  children.forEach(child => console.log(child, stats));
+function listChildren(client, path) {
+  client.getChildren(
+    path,
+    function(event) {
+      console.log("Got watcher event: %s", event);
+      listChildren(client, path);
+    },
+    function(error, children, stat) {
+      if (error) {
+        console.log(
+          "Failed to list children of node: %s due to: %s.",
+          path,
+          error
+        );
+        return;
+      }
 
-  if (err) {
-    console.log(err);
-  }
+      console.log("Children of node: %s are: %j.", path, children);
+    }
+  );
+}
+
+client.once("connected", function() {
+  console.log("Connected to ZooKeeper.");
+  listChildren(client, path);
 });
+
+client.connect();
