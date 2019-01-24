9da54ccd9b63c8ad8e2ca4e5063bcfc7814bc480
John Bull <91671@savemart.com>
2019-01-23 16:16:15 -0800

    changed payload to just message

--- consumer.js
+++ consumer.js
@@ -18,7 +18,7 @@ consumer.on("message", function(message, err) {
     console.log(err);
   } else {
     console.log("Here is the kafka message... " + JSON.stringify(message));
-    console.log(message.messages + ": ");
+    console.log(message.value + ": ");
   }
 });
 
